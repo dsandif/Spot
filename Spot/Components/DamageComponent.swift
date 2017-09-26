cd99e7eac7718eb357bbadb4e4b229d62cefffc0
Darien Sandifer <dsandif@gmail.com>
2017-09-26 09:13:09 -0500

    Added Damage component class
    Damage component affects health of other entities

---
+++ Spot/Components/DamageComponent.swift
@@ -0,0 +1,29 @@
+//
+//  DamageComponent.swift
+//  Spot
+//
+//  Created by Darien Sandifer on 9/26/17.
+//  Copyright © 2017 Darien Sandifer. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+class DamageComponent: GKComponent {
+    var power:Int = 0
+    
+    init(power:Int){
+        super.init()
+        self.power = power;
+    }
+    
+    func damaging(target:GKEntity){
+        let targetHealth = target.componentForClass(HealthComponent);
+        targetHealth.updateHealth(-self.power);
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
